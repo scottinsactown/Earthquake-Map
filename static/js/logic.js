e69f6d14dfcbdb39a1a3132bdda678206029e77c
Scott Clark <scottinsacto@yahoo.com>
2020-02-07 18:14:49 -0800

    circles layer visible

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,70 +4,57 @@ let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
 
 // Perform a GET request to the query URL
 d3.json(url,function(data) {
-      // Once we get a response, send the data.features object to the createFeatures function
     createFeatures(data.features)
 })
-// above ok to do first?
 
 function createFeatures(earthquakeData) {
-  // Function to run once for each feature in the features array
-  // let quakes = earthquakeData.features;
+  // Function to add circles to map
 
-  // // Initialize an array to hold bike markers
-  // let quakeMarkers = [];
+console.log(earthquakeData.length)
+let markers = [];
+for (let index = 0; index < earthquakeData.length; index++) {
+  let earthquake = earthquakeData[index];
+  let marker = L.circle([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]], { 
+    fillOpacity: 0.75,
+    //Adjust color based on mag
+    color: quakeColor(earthquake.properties.mag),
+    fillColor: quakeColor(earthquake.properties.mag),
+    // Adjust radius based on mag
+    radius: circleSize(earthquake.properties.mag)
+  }).bindPopup(
+    "<h4>"+earthquake.properties.place+"<h4>"
+    + "<h4> Magnitude: " + earthquake.properties.mag + "</h4>"
+    + "<h5>" + new Date(earthquake.properties.time) + "</h5>")
 
-  // for (let index = 0; index < quakes.geometry.coordinates; index++) {
-  //   let quake = quakes[index];
-
-  //       // For each station, create a marker and bind a popup with the station's name
-  //       let quakeMarker = L.marker([station.lat, station.lon])
-  //       .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "<h3>");
-  
-  //     // Add the marker to the bikeMarkers array
-  //     bikeMarkers.push(bikeMarker);
-
-  // Give each feature a popup describing the place and magnitude of the earthquake
-  function onEachFeature(feature, layer) {
-    let color="";
-    L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], { //coordinates in brackets - unpack?
-      fillOpacity: 0.75,
-      //Adjust color based on mag
-      color: "yellow",
-      fillColor: color,
-      // Adjust radius based on mag
-      radius: feature.properties.mag
-    });
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.mag) + "</p>"); // Meets project requirements???
-    }
-
-// //
-//   for (var i = 0; i < countries.length; i++) {
-
-//     // Conditionals for countries points
-//     var color = "";
-//     if (countries[i].points > 200) {
-//       color = "yellow";
-//     }
-//     else if (countries[i].points > 100) {
-//       color = "blue";
-//     }
-//     else if (countries[i].points > 90) {
-//       color = "green";
-//     }
-//     else {
-//       color = "red";
-//     }
-  
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  let earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-    // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes);
+    markers.push(marker);
 }
+
+  // function onEachFeature(feature, layer) {
+  //   L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], { 
+  //     fillOpacity: 0.75,
+  //     //Adjust color based on mag
+  //     color: quakeColor(feature.properties.mag),
+  //     fillColor: quakeColor(feature.properties.mag),
+  //     // Adjust radius based on mag
+  //     radius: circleSize(feature.properties.mag)
+  //   });
+  
+  //   // Give each feature a popup describing the place and magnitude and date of the earthquake
+  //   layer.bindPopup(
+  //         "<h4>"+feature.properties.place+"<h4>"
+  //         + "<h4> Magnitude: " + feature.properties.mag + "</h4>"
+  //         + "<h5>" + new Date(feature.properties.time) + "</h5>")
+  //      }
+  
+  // // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // let earthquakes = L.geoJSON(earthquakeData, {
+  //   onEachFeature: onEachFeature
+  // });
+
+    // createMap(earthquakes);
+    createMap(L.layerGroup(markers));
+}
+
 
 function createMap(earthquakes) {
 
@@ -76,6 +63,16 @@ function createMap(earthquakes) {
         maxZoom: 18,
         id: "mapbox.light",
         accessToken: API_KEY
+      });
+
+    // Create our map, giving it the lightmap and earthquakes layers to display on load
+    let myMap = L.map("map", {
+        center: [
+          37.09, -95.71 // Adjust???
+        ],
+        zoom: 4,
+        layers: [lightmap, earthquakes],
+        scrollWheelZoom: false
       });
 
     // Define a baseMaps object to hold our base layers
@@ -88,20 +85,53 @@ function createMap(earthquakes) {
     Earthquakes: earthquakes
   };
 
-    // Create our map, giving it the lightmap and earthquakes layers to display on load
-    let myMap = L.map("map", {
-        center: [
-          37.09, -95.71 // Adjust???
-        ],
-        zoom: 4,
-        layers: [lightmap, earthquakes]
-      });
-
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
   // Add the layer control to the map
-  // L.control.layers(baseMaps, overlayMaps, {
-  //   collapsed: false
-  // }).addTo(myMap);
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: true
+  }).addTo(myMap);
 
 }
+
+function quakeColor(mag) {
+  return mag > 5 ? "red":
+  mag > 4 ? "orange":
+  mag > 3 ? "gold":
+  mag > 2 ? "yellow":
+  mag > 1 ? "yellowgreen":
+  "greenyellow";
+}
+
+function circleSize(mag) {
+  return mag *1000;
+}
+
+
+  // // Set up the legend
+  // var legend = L.control({ position: "bottomright" });
+  // legend.onAdd = function() {
+  //   var div = L.DomUtil.create("div", "info legend");
+  //   var limits = geojson.options.limits;
+  //   var colors = geojson.options.colors;
+  //   var labels = [];
+
+  //   // Add min & max
+  //   var legendInfo = "<h1>Median Income</h1>" +
+  //     "<div class=\"labels\">" + //escape clause allow "" within ""
+  //       "<div class=\"min\">" + limits[0] + "</div>" +
+  //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+  //     "</div>";
+
+  //   div.innerHTML = legendInfo;
+
+  //   limits.forEach(function(limit, index) {
+  //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  //   });
+
+  //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  //   return div;
+  // };
+
+  // // Adding legend to the map
+  // legend.addTo(myMap);
